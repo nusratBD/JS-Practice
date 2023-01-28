ac41aae42fba2123772d94b30fcc6a77611629e7
nusratBD <nusrat.hurain15@gmail.com>
2023-01-28 22:43:35 +0600

    while loop and largest value findings

---
+++ 1.test.js
@@ -0,0 +1,6 @@
+//While Loop
+let person = 0;
+while(person<7){
+    console.log('Roast Den Please', person);
+    person++;
+}
---
+++ 2.test.js
@@ -0,0 +1,13 @@
+//Find The Largest Value
+const a = 13;
+const b = 79;
+const c = 113;
+if(a>b && a>c){
+    console.log('a is the largest number.', a);
+}
+else if(b>a && b>c){
+    console.log('b is the largest number.', b);
+}
+else{
+    console.log('c is the largest number');
+}
