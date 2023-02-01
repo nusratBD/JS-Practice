a3cd7c4bd85d341f37aa912e451bbf6ddc377aed
nusratBD <nusrat.hurain15@gmail.com>
2023-02-02 02:45:37 +0600

    Average of an array elements

---
+++ A4.avg-array-element.js
@@ -0,0 +1,17 @@
+//01
+const numbers = [1, 4, 6, 7, 8];
+let sum = 0;
+for(let i=0; i<numbers.length; i++){
+sum = sum + numbers[i];
+}
+let avg = sum/numbers.length;
+console.log(avg);
+
+//02
+const numbers2 = [56, 78, 23, 92, 16];
+let sum2 = 0;
+for(let i=0; i<numbers2.length; i++){
+sum2 = sum2 + numbers2[i];
+}
+let avg2 = sum2/numbers2.length;
+console.log(avg2);
