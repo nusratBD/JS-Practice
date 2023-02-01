5da77124e0608595728c3d506f40a15ddbb0748c
nusratBD <nusrat.hurain15@gmail.com>
2023-02-02 02:01:41 +0600

    get and set Object property

---
+++ A0.Object/Object.js
@@ -0,0 +1,88 @@
+const shoppingCart = {
+    laptop: 1,
+    mouse: 1,
+    Keyboard: 3,
+    watch: 1,
+    pen: 5,
+    notebook: 3,
+    mobile: 1,
+    mug: 1
+}
+/*---------------------------------------------------
+                    get object property 
+----------------------------------------------------*/
+//Method: 01
+console.log(shoppingCart.Keyboard);
+
+//Method: 02
+console.log(shoppingCart['watch']);
+
+//Method: 03
+const property = 'notebook';
+console.log(shoppingCart[property]);
+console.log(shoppingCart.property);//undefined. Not applicable.
+
+//Method: 04 : Keys
+const keys = Object.keys(shoppingCart);
+console.log(keys);
+/* 
+output: [
+  'laptop',   'mouse',
+  'Keyboard', 'watch',
+  'pen',      'notebook',
+  'mobile',   'mug'
+]
+*/
+
+//Method: 05 : values
+const values = Object.values(shoppingCart);
+console.log(values);
+/* 
+output:
+[
+  1, 1, 3, 1,
+  5, 3, 1, 1
+]
+*/
+
+/* -------------------------------------------------
+                set object property
+---------------------------------------------------- */
+console.log(shoppingCart);
+/* 
+output:
+{
+  laptop: 1,
+  mouse: 1,
+  Keyboard: 3,
+  watch: 1,
+  pen: 5,
+  notebook: 3,
+  mobile: 1,
+  mug: 1
+}
+*/
+
+//Method: 01
+shoppingCart.laptop = 2;
+
+//Method: 02
+shoppingCart['mouse'] = 2;
+
+//Method: 03
+shoppingCart[property] = 7;
+
+console.log(shoppingCart);
+/* 
+output:
+{
+  laptop: 2,
+  mouse: 2,
+  Keyboard: 3,
+  watch: 1,
+  pen: 5,
+  notebook: 7,
+  mobile: 1,
+  mug: 1
+}
+*/
--- A3.function-result-in-function.js
+++ A3.function-result-in-function.js
@@ -5,6 +5,6 @@ function add (num1, num2){
 const result1 = add(10, 20);
 console.log(result1);
 const result2 = add(5, 27);
-console.log(result2);
+console.log(result2);;;;;
 const finalResult = add(result1, result2);
 console.log(finalResult);
